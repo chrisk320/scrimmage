259ee8adf7ea4a95ac185edc2701b1dc495500f9
Christian Kim <chris.kim320@gmail.com>
2025-01-07 02:48:12 -0800

    mern stack practice

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+import cors from "cors";
+import path from 'path';
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use(
+    cors({
+      origin: "http://localhost:5173", // Allow requests from frontend
+      methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    })
+  );
+
+
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log('Server started at http://localhost:' + PORT);
+});
+
---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </StrictMode>
+)
